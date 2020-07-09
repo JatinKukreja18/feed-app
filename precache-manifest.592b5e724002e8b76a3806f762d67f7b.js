80b85b484ac23e4ddb8fe496915eb664d46a2d89
jatin kukreja <jatinkukreja18@yahoo.in>
2020-07-09 08:24:41 +0530

    Updates

---
+++ precache-manifest.592b5e724002e8b76a3806f762d67f7b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9095939e6c57be8702a0a678ed8f4e58",
+    "url": "/feed-app/index.html"
+  },
+  {
+    "revision": "4eec779d1d1fffc4c9ec",
+    "url": "/feed-app/static/css/2.c27a359e.chunk.css"
+  },
+  {
+    "revision": "50b5bdd228758f369611",
+    "url": "/feed-app/static/css/main.700a81dd.chunk.css"
+  },
+  {
+    "revision": "4eec779d1d1fffc4c9ec",
+    "url": "/feed-app/static/js/2.58989253.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/feed-app/static/js/2.58989253.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50b5bdd228758f369611",
+    "url": "/feed-app/static/js/main.a4b1d1d7.chunk.js"
+  },
+  {
+    "revision": "09f4ff44e35aeb9777e0",
+    "url": "/feed-app/static/js/runtime-main.7aeddc88.js"
+  }
+]);
